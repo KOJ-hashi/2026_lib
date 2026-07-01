6ae0d65562aee21c223586769256f02b142df1de
KOJ-hashi <s.nitkagoshima@gmail.com>
2026-07-01 16:48:56 +0900

    Refactor gm6020 class methods and variables

--- gm6020/gm6020.h
+++ gm6020/gm6020.h
@@ -1,3 +1,4 @@
+//Support by kashidawa
 #ifndef INCLUDED_gm6020_H
 #define INCLUDED_gm6020_H
 
@@ -16,19 +17,23 @@ class gm6020 : public CANReceiver {
         CANMessage gm6020_can_msg;
         gm6020(RawCAN &can,int motor_num);
         int gm6020_send(int* moter);
-        void rbms_read(CANMessage &msg, short *rotation,short *speed);
+        void rbms_read(CANMessage &msg, short *rotation,short*speed);
         bool handle_message(const CANMessage &msg) override;
         void can_read(){}
         const vector<int>& getSumDeg() const { return _sum_deg; }
-        float pid(float T,short rpm_now, short set_speed,float *delta_rpm_pre,float *ie,float KP=150,float KI=70, float KD=0);
+         bool has_received() const { return _has_received; }
+        float pid(float T,short rpm_now, short set_speed,float *delta_rpm_pre,float*ie,float KP=150,float KI=70, float KD=0);
         void deg_control(float* set_deg,int* motor,float* KP_GM6020,float* KI_GM6020,float* KD_GM6020);
-        void pos_control(float *set_deg, int *output, float Kp, float Ki, float Kd);
+        void pos_control(float *set_deg, int*output, float Kp, float Ki, float Kd);
         float get_rotation() { return _rotation; }
+        float gm_get_rotation(){return gm_rotation;}
         int _rotation;
+        int gm_rotation;
         int _delta_deg;
         int now_pos;
     private:
         static constexpr float pi = 3.1415926535;
+        bool _has_received = false;
         CANMessage _canMessage,_canMessage2,_msg;
         RawCAN &_can;
         vector<int> _sum_deg;
@@ -38,11 +43,12 @@ class gm6020 : public CANReceiver {
         int _torque;
         int _temperature;
         int set_deg;
-        
-        
+
+
 
 
 };
+
 
 
 #endif
